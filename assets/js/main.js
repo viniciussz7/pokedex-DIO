6eb39b5ddd20b45259f251430d8d268f7072ff9d
viniciussouza <viniciusouza7@gmail.com>
2023-08-31 21:24:17 -0300

    consuming pokeAPI and creating dynamic list in html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,34 @@
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
+function convertListPokemonToLiHtml (pokemon) {
+    return `
+        <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">"${pokemon.name}"</span>
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">posion</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                </div>
+        </li>
+    `
+}
 
+const pokemonList = document.getElementById("pokemonList")
 
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+
+    .then((pokemons) => {
+
+        pokemons.forEach(pokemon => {
+            pokemonList.innerHTML += convertListPokemonToLiHtml(pokemon)
+        })
+
+    })
+    .catch((error) => console.log(error))

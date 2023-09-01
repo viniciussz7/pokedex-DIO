7f66e1d7e61390ef3e00ec783fb9d4f93fdb47d3
viniciussouza <viniciusouza7@gmail.com>
2023-09-01 12:48:34 -0300

    changing to handle multiple requests and update pokemon list with datails

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
-function convertListPokemonToLiHtml (pokemon) {
+function convertPokemonsTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((types) => `<li class="type">${types.type.name}</li>`)
+}
+
+function convertListPokemonToLiHtml(pokemon) {
     return `
         <li class="pokemon">
-                <span class="number">#001</span>
+                <span class="number">#${pokemon.order}</span>
                 <span class="name">"${pokemon.name}"</span>
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">posion</li>
+                        ${convertPokemonsTypesToLi(pokemon.types).join("")}
                     </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                    <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
                 </div>
         </li>
     `
@@ -19,4 +22,3 @@ const pokemonList = document.getElementById("pokemonList")
 pokeApi.getPokemons().then((pokemons = []) => {
     pokemonList.innerHTML += pokemons.map(convertListPokemonToLiHtml).join("") //listPokemonsJson -> listPokemonsHtml no separation
 })
-    
